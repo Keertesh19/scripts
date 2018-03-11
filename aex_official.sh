e0c8cb1c9fbb6f001a3d7b129dd2937a15ba8d00
Keertesh <keertesh.k98@gmail.com>
2018-03-11 08:19:57 +0000

    Use GPU while building.

--- aex_official.sh
+++ aex_official.sh
@@ -2,6 +2,7 @@
 
 . b*/e*
 export EXTENDED_BUILD_TYPE=OFFICIAL
+export USE_GPU_FOR_BUILDS=true
 lunch aosp_sanders-userdebug
 make installclean
 make bacon -j32
--- cosmic_official.sh
+++ cosmic_official.sh
@@ -3,6 +3,7 @@
 . b*/e*
 export KBUILD_BUILD_HOST=Saturn
 export KBUILD_BUILD_USER=Savitar
+export USE_GPU_FOR_BUILDS=true
 lunch cos_sanders-userdebug
 make installclean
 make bacon -j32
