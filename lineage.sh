8b1733b67c4bb83e3ad692334a42cb5202eef08a
Keertesh <keertesh.k98@gmail.com>
2018-06-03 17:19:00 +0530

    LineageOS Shits

---
+++ lineage.sh
@@ -0,0 +1,5 @@
+#!/bin/bash/
+
+. b*/e*
+lunch lineage_sanders-userdebug
+make bacon -j32 && sudo shutdown
---
+++ lineage_manifest.sh
@@ -0,0 +1,7 @@
+#!/bin/bash/
+
+git clone https://github.com/Keertesh19/device_motorola_sanders device/motorola/sanders
+git clone https://github.com/Keertesh19/kernel_motorola_msm8953 kernel/motorola/msm8953
+git clone https://github.com/BigTopKrazies/vendor_motorola_sanders vendor/motorola/sanders
+git clone https://github.com/jhenrique09/prebuilts_clang_host_linux-x86_7.0-DragonTC prebuilts/clang/host/linux-x86/7.0-DragonTC
+git clone https://github.com/Keertesh19/prebuilts_gcc_linux-x86_aarch64_aarch64-linaro-7.2
