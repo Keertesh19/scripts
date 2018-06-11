08cc1f377be495decca477b068b3b90dc8d3054a
Keertesh <keertesh.k98@gmail.com>
2018-06-11 07:41:10 +0000

    Add devicesettings

--- lineage_manifest.sh
+++ lineage_manifest.sh
@@ -5,3 +5,4 @@ git clone https://github.com/Keertesh19/kernel_motorola_msm8953 kernel/motorola/
 git clone https://github.com/BigTopKrazies/vendor_motorola_sanders vendor/motorola/sanders
 git clone https://github.com/jhenrique09/prebuilts_clang_host_linux-x86_7.0-DragonTC prebuilts/clang/host/linux-x86/7.0-DragonTC
 git clone https://github.com/Keertesh19/prebuilts_gcc_linux-x86_aarch64_aarch64-linaro-7.2
+git clone https://github.com/LineageOS/android_packages_resources_devicesettings packages/resources/devicesettings
