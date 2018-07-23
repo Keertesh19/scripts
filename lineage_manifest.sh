1b94761320ce8bc3abb82e69589e5501b27f904c
Keertesh <keertesh.k98@gmail.com>
2018-07-23 11:26:11 +0530

    Update lineage_manifest.sh

--- lineage_manifest.sh
+++ lineage_manifest.sh
@@ -1,8 +1,7 @@
 #!/bin/bash/
 
-git clone https://github.com/Keertesh19/device_motorola_sanders device/motorola/sanders
-git clone https://github.com/Keertesh19/kernel_motorola_msm8953 kernel/motorola/msm8953
-git clone https://github.com/BigTopKrazies/vendor_motorola_sanders vendor/motorola/sanders
+git clone https://github.com/Keertesh19/device_motorola_sanders -b los-exp device/motorola/sanders
+git clone https://github.com/GZR-Kernels/kernel_motorola_msm8953_sanders -b 8.1-ods kernel/motorola/msm8953
+git clone https://github.com/Keertesh19/vendor_motorola_sanders vendor/motorola/sanders
 git clone https://github.com/jhenrique09/prebuilts_clang_host_linux-x86_7.0-DragonTC prebuilts/clang/host/linux-x86/7.0-DragonTC
-git clone https://github.com/Keertesh19/prebuilts_gcc_linux-x86_aarch64_aarch64-linaro-7.2
 git clone https://github.com/LineageOS/android_packages_resources_devicesettings packages/resources/devicesettings
