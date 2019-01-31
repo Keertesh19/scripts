b282192abe909475c89a3e7d203e9b0f095c9ff4
Keertesh <keertesh.k98@gmail.com>
2019-01-31 11:46:45 +0530

    PIE please

--- sanders.sh
+++ sanders.sh
@@ -1,7 +1,7 @@
 #!bin/bash/
 
-git clone https://github.com/GZR-Kernels/kernel_motorola_msm8953.git -b 8.1-ods kernel/motorola/msm8953
-git clone https://github.com/GZOSP-Devices/device_motorola_sanders.git -b 8.1 device/motorola/sanders
-git clone https://github.com/BigTopKrazies/vendor_motorola_sanders.git -b 8.1 vendor/motorola/sanders
-git clone https://github.com/LineageOS/android_packages_resources_devicesettings.git -b lineage-15.0 packages/resources/devicesettings
-
+git clone git@github.com:Keertesh19/android_device_motorola_sanders.git device/motorola/sanders
+git clone git@github.com:therootlord/android_kernel_motorola_msm8953.git -b 9.0-eas-caf kernel/motorola/msm8953
+git clone git@github.com:therootlord/proprietary_vendor_motorola_sanders.git -b 9.0-test2 vendor/motorola/sanders
+git clone git@github.com:LineageOS/android_packages_resources_devicesettings.git packages/resources/devicesettings
+git clone git@github.com:jhenrique09/prebuilts_clang_host_linux-x86_7.0-DragonTC.git prebuilts/clang/host/linux-x86/7.0-DragonTC
