d85478be7449719ca7cebac352b1374fdeb77558
Keertesh <keertesh.k98@gmail.com>
2020-04-14 15:34:05 +0530

    Update whyred_common.sh

--- whyred_common.sh
+++ whyred_common.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/Keertesh19/device_xiaomi_sdm660-common -b ten device/xiaomi/sdm660-common
-git clone https://github.com/PixelExperience-Devices/kernel_xiaomi_whyred -b ten kernel/xiaomi/whyred
+git clone https://github.com/xiaomi-sdm660-devs/android_kernel_xiaomi_sdm660 -b unified/lineage-17.1 kernel/xiaomi/sdm660
 git clone https://github.com/Keertesh19/device_xiaomi_whyred-common -b ten device/xiaomi/whyred
 git clone https://github.com/Keertesh19/vendor_xiaomi_whyred-common -b ten vendor/xiaomi/whyred
-git clone https://github.com/Keertesh19/android_vendor_xiaomi_sdm660-common -b ten vendor/xiaomi/sdm660-common
-git clone https://GitHub.com/PixelExperience-Devices/vendor_MiuiCamera -b ten-whyred vendor/MiuiCamera
+git clone https://github.com/Keertesh19/android_vendor_xiaomi_sdm660-common -b cr-8.0-eas vendor/xiaomi/sdm660-common
+git clone https://gitHub.com/PixelExperience-Devices/vendor_MiuiCamera -b ten-whyred vendor/MiuiCamera
